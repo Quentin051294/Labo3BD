0b541a95c894f83bc426b5cb66b6f238cd8e0875
Quentin <quentin_nico@hotmail.com>
2016-09-29 17:33:51 +0200

    Unit Test

---
+++ Labo3/UnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using Model;
+using System.Linq;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            Database.SetInitializer(new DbInitializer());
+            using (CompanyContext context = GetContext())
+            {
+                context.Database.Initialize(true);
+            }
+        }
+
+        [TestMethod]
+        public void insertionFonctionnelle()
+        {
+            using (var context = GetContext())
+            {
+                Assert.AreEqual(1, context.Customers.ToList().Count);
+            }
+        }
+
+        public CompanyContext GetContext()
+        {
+            return new CompanyContext();
+        }
+    }
+}
